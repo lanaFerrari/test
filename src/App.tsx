30676b6017b195d793e0c8948ded9fe75272748a
Lana Ferrari <ferrarirangel@gmail.com>
2022-03-14 20:16:43 +0000

    Inital commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
@@ -15,6 +15,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          
         >
           Learn React
         </a>
